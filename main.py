2169fb2b0834cea354a83449c557597ed85d0bb4
UgrasK <ugurkaraer1989@gmail.com>
2022-07-28 14:11:56 +0300

    added try/except/else handling to check wrong input types.

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # create a dictionary from dataframe with dict comprehension
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-# take user input
-word = input("Enter a word: ").upper()
+def generate_phonetic():
+    # take user input
+    word = input("Enter a word: ").upper()
 
-# show user input as nato alphabet
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+    # show user input as nato alphabet
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
